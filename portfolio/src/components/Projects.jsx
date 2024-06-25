d8f7cdb2b7fd560cd6c8792e74c8ab14b5c8deaf
iamprakhar27 <prakharofficial17@gmail.com>
2024-06-25 16:26:29 +0530

    Updated projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
                                 <NavLink to="#">
                                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Portfolio</h5>
                                 </NavLink>
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Brief description of the project highlighting key features and technologies used.</p>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Passionate Full-Stack Developer specializing in the MERN stack, with a keen eye for creating seamless user experiences and a drive to continually learn and innovate.</p>
                                 <NavLink to="https://github.com/iamprakhar27/MyPortfolio/" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 z-30">
                                     Learn More
                                     <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
@@ -27,8 +27,10 @@ function Projects() {
                             </div>
                             <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center p-5 rounded-lg">
                                 <div className="text-center text-white z-10">
-                                    <h5 className="mb-2 text-2xl font-bold">Portfolio</h5>
-                                    <p className="mb-3">Detailed project information, technologies used, and other relevant details.</p>
+                                    <h5 className="mb-2 text-2xl font-bold underline">Technologies Used</h5>
+                                    <p className="mb-3">
+                                    This project was created using the MERN stack, which includes MongoDB as a NoSQL database used for storing data, Express.js as a web application framework for Node.js, React.js as a JavaScript library for building user interfaces, and Node.js as a JavaScript runtime environment for server-side programming. 
+                                    </p>
                                 </div>
                             </div>
                         </div>
